9c9176a495b4944948f58f802e7a6f0eb05e5c4e
Mark Banner <mbanner@mozilla.com>
2013-09-15 14:22:06 -0700

    Rework the server to be more based on events and add code coverage reports

--- deploy_server.js
+++ deploy_server.js
@@ -1,4 +1,8 @@
-#!/usr/bin/env node
+const child_process = require('child_process'),
+spawn = child_process.spawn;
+
+const masterRepoUrl = 'git://github.com/mozilla/talkilla';
+const deployServer = 'talkilla-stage.mozillalabs.com';
 
 var express = require('express'),
     util = require('util'),
@@ -14,6 +18,34 @@ var latestSha = null;
 
 console.log("deploy server starting up");
 
+function splitAndEmit(chunk, cb) {
+  if (chunk) chunk = chunk.toString();
+  if (typeof chunk === 'string') {
+    chunk.split('\n').forEach(function (line) {
+      line = line.trim();
+      if (line.length) cb(line);
+    });
+  }
+}
+
+function spawnCommand(dply, cmd, params, pwd, cb) {
+  var p = spawn(cmd, params, {
+    cwd: pwd
+  });
+
+  var pr = function (d) {
+    dply.emit('progress', d);
+  };
+
+  p.stdout.on('data', function(c) { splitAndEmit(c, pr); });
+  p.stderr.on('data', function(c) { splitAndEmit(c, pr); });
+
+  p.on('exit', function(code, signal) {
+    return cb(code != 0);
+  });
+};
+
+
 function Deployer() {
   events.EventEmitter.call(this);
 
@@ -28,124 +60,160 @@ function Deployer() {
 
   this._shaFile = path.join(this._dataDir, 'lastSha');
 
-  var self = this;
-
   git.init(this._codeDir, function(err) {
     console.log("init code is ", err);
     if (err) {
       console.log("can't init code dir:", err);
       process.exit(1);
     }
-    self.emit('ready');
-  });
+    this.emit('ready');
+  }.bind(this));
+
+  this.on('checkForUpdates', this._onCheckForUpdates.bind(this));
+  this.on('pullLatest', this._onPullLatest.bind(this));
+  this.on('deployNewCode', this._onDeployNewCode.bind(this));
+  this.on('completeUpdate', this._onCompleteUpdate.bind(this));
+  this.on('runCodeCoverage', this._onRunCodeCoverage.bind(this));
+  this.on('makeCoverServer', this._onMakeCoverServer.bind(this));
+  this.on('finished', this._onFinished.bind(this));
 }
 
 util.inherits(Deployer, events.EventEmitter);
 
-Deployer.prototype._completeUpdate = function(cb) {
-  var self = this;
-
-  irc.send("deployment of " + latestSha + " to talkilla-stage completed successfully");
-  irc.disconnect();
-
-  fs.writeFile(self._shaFile, latestSha, function(err) {
-    if (err)
-      return cb;
-    self.emit('info', 'completed update');
-    self._busy = false;
-  });
-};
-
-Deployer.prototype._deployNewCode = function(cb) {
-  var self = this;
-
-  self.emit('info', 'pushing to server');
-  git.push(this._codeDir, 'talkilla-stage.mozillalabs.com',
-    function (d) {
-      self.emit('progress', d);
-    },
-    function(res) {
-      if (res)
-        return cb(res);
-
-      self.emit('info', 'push successful');
-      self._completeUpdate(cb);
-    }
-  );
-}
-
-Deployer.prototype._pullLatest = function(cb) {
-  var self = this;
-  git.pull(this._codeDir, 'git://github.com/mozilla/talkilla', 'master', function(l) {
-    self.emit('progress', l);
-  }, function(err) {
-    if (err)
-      return cb(err);
-
-    git.currentSHA(self._codeDir, function(err, latest) {
-      latestSha = latest;
-      self.emit('info', 'latest available sha is ' + latestSha);
-
-      fs.readFile(self._shaFile, function(err, data) {
-        if (!err) {
-          self.emit('info', 'last sha is ' + data);
-          if (data != latestSha) {
-            self.emit('info', 'update required');
-          }
-          else {
-            self.emit('info', 'no update required');
-            self._busy = false;
-            return;
-          }
-        }
-        else {
-          self.emit('info', 'no lastSha file, assuming update necessary (err was ' + err + ')');
-        }
-        self._deployNewCode(cb);
-      });
-    });
-  });
-}
-
-Deployer.prototype.checkForUpdates = function() {
-  var self = this;
-
+Deployer.prototype._onCheckForUpdates = function() {
   if (this._busy)
     return;
 
   this._busy = true;
-  self.emit('info', 'checking for updates');
+  this.emit('info', 'checking for updates');
+  this.emit('pullLatest');
+};
 
-  self._pullLatest(function(err, sha) {
-    if (err)
-      self.emit('error', err);
+Deployer.prototype._onPullLatest = function() {
+  git.pull(this._codeDir, masterRepoUrl, 'master',
+    function(l) {
+      this.emit('progress', l);
+    }.bind(this),
+    function(err) {
+      if (err) {
+        this.emit('finished', true, true, 'Error pulling latest whilst getting sha');
+        return;
+      }
 
-    irc.send("deployment of " + latestSha + " failed :-( - please poke Standard8");
-    irc.disconnect();
+      git.currentSHA(self._codeDir, function(err, latest) {
+        latestSha = latest;
+        this.emit('info', 'latest available sha is ' + latestSha);
 
-    self._busy = false;
+        fs.readFile(this._shaFile, function(err, data) {
+          if (!err) {
+            this.emit('info', 'last sha is ' + data);
+            if (data != latestSha) {
+              this.emit('info', 'update required');
+            }
+            else {
+              this.emit('finished', false, false, 'no update required');
+              return;
+            }
+          }
+          else {
+            this.emit('info', 'no lastSha file, assuming update necessary (err was ' + err + ')');
+          }
+          this.emit('deployNewCode');
+          this.emit('runCodeCoverage');
+      });
+    });
+  }.bind(this));
+};
+
+Deployer.prototype._onDeployNewCode = function() {
+  this.emit('info', 'pushing to server');
+  git.push(this._codeDir, deployServer,
+    function (d) {
+      this.emit('progress', d);
+    },
+    function(res) {
+      if (res) {
+        this.emit('finished', true, true, 'Error deploying code to staging server');
+        return;
+      }
+
+      this.emit('info', 'push successful');
+      this.emit('completeUpdate');
+    }
+  );
+};
+
+Deployer.prototype._onCompleteUpdate = function() {
+
+  fs.writeFile(self._shaFile, latestSha, function(err) {
+    if (err) {
+      this.emit('finished', true, true, "completed update, but couldn't write latestSha file");
+      return;
+    }
+
+    this.emit('finished', false, true, "deployment of " + latestSha + " to talkilla-stage completed successfully");
   });
 };
 
+Deployer.prototype._onRunCodeCoverage = function() {
+  var coverDir = __dirname + "/data/code";
+
+  // Make install to run the data
+  spawnCommand(self, 'make', ['install'], coverDir, function(err) {
+    if (err) {
+      this.emit('finished', true, true, "Error during make install for code coverage report", true);
+      return;
+    }
+
+    this.emit('makeCoverServer');
+  });
+};
+
+Deployer.prototype._onMakeCoverServer = function() {
+  var coverDir = __dirname + "/data/code";
+
+  spawnCommand(self, 'make', ['cover_server'], coverDir, function(err) {
+    if (err) {
+      this.emit('finished', true, true, "Error making code coverage report", true);
+      return;
+    }
+    this.emit('info', "Finished code coverage");
+  });
+};
+
+Deployer.prototype._onFinished = function(err, irc, msg, fakeFinished) {
+  if (!fakeFinished)
+    this._busy = false;
+
+  irc = false;
+
+  if (irc) {
+    if (err)
+      irc.send(msg + " :-( - please poke Standard");
+    else
+      irc.send(msg);
+  }
+
+  if (err)
+    this.emit('error', msg);
+  else
+    this.emit('info', msg);
+
+  if (irc)
+    irc.disconnect();
+};
+
 var deployer = new Deployer();
-
-var currentLogFile = null;
-
-console.log("deployment log dir is:", '.');
 
 [ 'info', 'ready', 'error', 'deployment_begins', 'deployment_complete', 'progress' ].forEach(function(evName) {
   deployer.on(evName, function(data) {
     if (data !== null && data !== undefined && typeof data != 'string') data = JSON.stringify(data, null, 2);
     var msg = evName + (data ? (": " + data) : "");
     console.log(msg);
-    if (currentLogFile)
-      currentLogFile.write(msg + "\n");
   });
 });
 
 deployer.on('ready', function() {
-  //deployer.checkForUpdates();
-
   var server = http.createServer(app);
 
   app.get('/', function(req, res) {
@@ -156,14 +224,17 @@ deployer.on('ready', function() {
   });
 
   app.get('/check', function(req, res) {
-    deployer.checkForUpdates();
+    deployer.emit('checkForUpdates');
     res.send('ok');
   });
 
   app.post('/check', function(req, res) {
-    deployer.checkForUpdates();
+    deployer.emit('checkForUpdates');
     res.send('ok');
   });
+
+  app.use("/coverage", express.static(__dirname + "/data/code/coverage/lcov-report"));
+
 
   // Check for updates every hour for now.
   /*
